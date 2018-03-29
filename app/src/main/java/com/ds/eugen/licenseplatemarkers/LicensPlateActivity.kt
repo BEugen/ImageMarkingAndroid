d0d93b960df0907bf353f1a354b32e2a4403f455
eugen <b-eugen@yandex.ru>
2018-03-29 23:21:11 +0300

    add

---
+++ app/src/main/java/com/ds/eugen/licenseplatemarkers/LicensPlateActivity.kt
@@ -0,0 +1,12 @@
+package com.ds.eugen.licenseplatemarkers
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LicensPlateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_licens_plate)
+    }
+}
